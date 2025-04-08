07a99884d33617d45073f1959849b245387d1797
23Siya <Siyabongamatsheme@gmail.com>
2025-04-08 22:33:44 +0200

    Feature Adding Footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+
+    return (
+        <footer className="bg-gray-800 text-white py-4 text-center">
+        <p>&copy; 2023 Siya's Movie App. All rights reserved.</p>
+        </footer>
+    );
+}
+export default Footer;  
